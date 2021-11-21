e758aff45fcf05dd83815c190f831016c8b82216
sureshasoori <sureshasoori60@gmail.com>
2021-11-21 19:30:54 +0530

    mon

--- pattern1.js
+++ pattern1.js
@@ -1,8 +1,14 @@
 var n = 7;
-var p = document.getElementById("p");
+//var p = document.getElementById("p");
 for(var i=0;i<n;i++){
 
     for(var j=0;j<=7;j++){
-        p.innerText = "* ";
-    }  
+        console.log("* ");
+        //document.write("* ")
+        //p.innerText = "* ";
+    } 
+    console.log("\n");
+
+    //document.write("\n");
+    //p.innerText = "\n";
 }
